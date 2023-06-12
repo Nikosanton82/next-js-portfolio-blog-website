867edd69ba2004ff9d797922ce14fdb2aef860bc
Nikos <nikosant82@gmail.com>
2023-06-12 08:07:46 +0200

    Google Auth Sign In

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import useSWR from "swr";
 
 import styles from "./page.module.css";
+import { useSession } from "next-auth/react";
 
 const Dashboard = () => {
   // const [data, setData] = useState([]);
@@ -28,14 +29,15 @@ const Dashboard = () => {
   //   getData();
   // }, []);
 
+  const session = useSession();
+  console.log(session);
+
   const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
   const { data, error, isLoading } = useSWR(
     "https://jsonplaceholder.typicode.com/posts",
     fetcher
   );
-
-  console.log(data);
 
   return <div className={styles.container}>Dashboard</div>;
 };
